076d4ebda180ca5f50c51ca140e90482aa3e5388
Keith Kurak <keith.kurak@gmail.com>
2025-10-06 00:02:23 -0400

    hiding tab bar works

--- app/(logged-in)/(tabs)/_layout.tsx
+++ app/(logged-in)/(tabs)/_layout.tsx
@@ -1,13 +1,28 @@
 import { TabBarIcon } from "@/components/TabBarIcon";
-import { Tabs } from "expo-router";
+import { Tabs, useSegments } from "expo-router";
+import { findIndex } from "lodash";
 import colors from "../../../constants/colors";
 
 export default function Layout() {
+
+  const segments = useSegments();
+
+  const tabRouteIndex = findIndex(
+    segments,
+    // @ts-ignore
+    (s) => s === "buckets" || s === "settings"
+  );
+
+  const hideTabs =
+    tabRouteIndex !== -1 && // the route exists
+    tabRouteIndex !== segments.length - 1 // the route isn't at the end
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
         tabBarActiveTintColor: colors.tint,
+        tabBarStyle: { display: hideTabs ? "none" : "flex" },
       }}
     >
       <Tabs.Screen
